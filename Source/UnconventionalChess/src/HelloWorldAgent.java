b58afc93ab9625c83587df93657a8f6dab9829ea
Jordan Mackie <jmackie97@hotmail.com>
2019-09-18 18:11:57 +0100

    got argument parsing working

--- Source/UnconventionalChess/src/HelloWorldAgent.java
+++ Source/UnconventionalChess/src/HelloWorldAgent.java
@@ -6,5 +6,13 @@ public class HelloWorldAgent extends Agent {
         System.out.println("My local name is " + getAID().getLocalName());
         System.out.println("My GUID is " + getAID().getName());
         System.out.println("My addresses are " + String.join(",", getAID().getAddressesArray()));
+
+        Object[] args = getArguments();
+        System.out.println("My arguments are:");
+        if (args != null) {
+            for (Object arg : args) {
+                System.out.println("" + arg);
+            }
+        }
     }
 }
