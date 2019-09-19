1bcaa1cec55ce111c0f4bf2b6326e7993424e89a
Jordan Mackie <jmackie97@hotmail.com>
2019-09-19 19:43:08 +0100

    added gateway spring boot

---
+++ Source/UnconventionalChess/multiagentchessgateway/src/main/java/stacs/chessgateway/ChessgatewayApplication.java
@@ -0,0 +1,13 @@
+package stacs.chessgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessgatewayApplication.class, args);
+    }
+
+}
