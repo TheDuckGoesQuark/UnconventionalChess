5e8ef29b4feb249abb7b6826a49df91e3ebd15a0
Jordan Mackie <jmackie97@hotmail.com>
2019-09-21 00:52:18 +0100

    added some explanatory comments

--- Source/UnconventionalChess/multiagentchessgateway/src/main/java/stacs/chessgateway/config/WebSocketConfig.java
+++ Source/UnconventionalChess/multiagentchessgateway/src/main/java/stacs/chessgateway/config/WebSocketConfig.java
@@ -18,15 +18,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         // set prefix for websocket endpoints to '/app'
+        // i.e. methods annotated with @MessageMapping will only be triggered
+        // if the client prefixes their request destination with this string
         config.setApplicationDestinationPrefixes("/app");
-        // creates in-memory broker for sending messsages to clients
+
+        // creates in-memory broker for sending messages to clients
+        // i.e. subscribable topics must have this prefix
         config.enableSimpleBroker("/topic");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // registers '/chess' as a STOMP endpoint
-        // and adds SockJS as a fallback for when WebSockets are not supported
+        // registers '/ws' as a STOMP endpoint
+        // and adds SockJS as a fallback for when WebSockets are not supported.
+        // This is the URL that will be used to perform the initial handshake for STOMP comms
         registry.addEndpoint("/ws").withSockJS();
     }
 }
