5fd0ea187ced05af9192a2d06bc8eb1cbb77cd36
Jordan Mackie <jmackie97@hotmail.com>
2019-09-21 00:40:59 +0100

    removed unecessary import

--- Source/UnconventionalChess/multiagentchessgateway/src/main/java/stacs/chessgateway/controllers/WebsocketController.java
+++ Source/UnconventionalChess/multiagentchessgateway/src/main/java/stacs/chessgateway/controllers/WebsocketController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
