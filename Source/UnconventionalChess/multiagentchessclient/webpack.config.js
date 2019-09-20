f2a91995790f300ae30d97831324a82663012c74
Jordan Mackie <jmackie97@hotmail.com>
2019-09-20 17:42:07 +0100

    fixed board not loading

--- Source/UnconventionalChess/multiagentchessclient/webpack.config.js
+++ Source/UnconventionalChess/multiagentchessclient/webpack.config.js
@@ -20,7 +20,11 @@ module.exports = {
                         presets: [
                             "@babel/preset-env",
                             "@babel/preset-react",
-                            {"plugins":["@babel/plugin-proposal-class-properties"]}
+                            {
+                                "plugins": [
+                                    "@babel/plugin-proposal-class-properties"
+                                ]
+                            }
                         ]
                     }
                 }]
