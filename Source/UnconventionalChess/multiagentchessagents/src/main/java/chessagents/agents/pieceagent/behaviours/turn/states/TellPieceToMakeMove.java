9fccf133cfa279896b0a93e1ca1a21e732194e9e
Jordan Mackie <jmackie97@hotmail.com>
2019-11-01 01:28:30 +0000

    implemented tell piece to make move

--- Source/UnconventionalChess/multiagentchessagents/src/main/java/chessagents/agents/pieceagent/behaviours/turn/states/TellPieceToMakeMove.java
+++ Source/UnconventionalChess/multiagentchessagents/src/main/java/chessagents/agents/pieceagent/behaviours/turn/states/TellPieceToMakeMove.java
@@ -3,9 +3,74 @@ package chessagents.agents.pieceagent.behaviours.turn.states;
 import chessagents.agents.pieceagent.PieceContext;
 import chessagents.agents.pieceagent.behaviours.turn.TurnContext;
 import chessagents.agents.pieceagent.pieces.PieceAgent;
-import jade.core.behaviours.Behaviour;
+import chessagents.ontology.ChessOntology;
+import chessagents.ontology.schemas.actions.MakeMove;
+import chessagents.ontology.schemas.concepts.Move;
+import chessagents.ontology.schemas.concepts.Piece;
+import chessagents.ontology.schemas.concepts.Position;
+import jade.content.lang.Codec;
+import jade.content.lang.sl.SLVocabulary;
+import jade.content.onto.OntologyException;
+import jade.content.onto.basic.Action;
+import jade.core.AID;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+import jade.util.Logger;
 
-public class TellPieceToMakeMove extends Behaviour {
+public class TellPieceToMakeMove extends SimpleBehaviour {
+    private final Logger logger = Logger.getMyLogger(getClass().getName());
+    private final PieceContext pieceContext;
+    private final TurnContext turnContext;
+
     public TellPieceToMakeMove(PieceAgent pieceAgent, PieceContext pieceContext, TurnContext turnContext) {
+        super(pieceAgent);
+        this.pieceContext = pieceContext;
+        this.turnContext = turnContext;
+    }
+
+    @Override
+    public void action() {
+        pieceContext.getBoard().getRandomMove().ifPresent(move -> {
+            var movingPiece = getAIDOfPieceAtPosition(move.getSource());
+            var request = createRequestToMove(movingPiece, move);
+            sendRequestMove(request);
+        });
+    }
+
+    private void sendRequestMove(ACLMessage request) {
+        pieceContext.getAidToPiece().values().stream().map(Piece::getAgentAID)
+                .forEach(request::addReceiver);
+
+        myAgent.send(request);
+    }
+
+    private ACLMessage createRequestToMove(AID movingPiece, Move move) {
+        var request = new ACLMessage(ACLMessage.REQUEST);
+        request.setOntology(ChessOntology.ONTOLOGY_NAME);
+        request.setLanguage(FIPANames.ContentLanguage.FIPA_SL);
+        var makeMove = new MakeMove(move);
+        var action = new Action(movingPiece, makeMove);
+
+        try {
+            myAgent.getContentManager().fillContent(request, action);
+        } catch (Codec.CodecException | OntologyException e) {
+            logger.warning("Failed to create make move request: " + e.getMessage());
+        }
+
+        return request;
+    }
+
+    private AID getAIDOfPieceAtPosition(Position position) {
+        return pieceContext.getAidToPiece().values().stream()
+                .filter(piece -> piece.getPosition().equals(position))
+                .map(Piece::getAgentAID)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public boolean done() {
+        return true;
     }
 }
--- Source/UnconventionalChess/multiagentchessagents/src/main/java/chessagents/agents/pieceagent/behaviours/turn/states/WaitForProposalRequest.java
+++ Source/UnconventionalChess/multiagentchessagents/src/main/java/chessagents/agents/pieceagent/behaviours/turn/states/WaitForProposalRequest.java
@@ -4,6 +4,10 @@ import chessagents.agents.pieceagent.PieceContext;
 import chessagents.agents.pieceagent.behaviours.turn.TurnContext;
 import chessagents.agents.pieceagent.behaviours.turn.fsm.PieceTransition;
 import chessagents.agents.pieceagent.pieces.PieceAgent;
+import chessagents.ontology.schemas.actions.MakeMove;
+import jade.content.lang.Codec;
+import jade.content.onto.OntologyException;
+import jade.content.onto.basic.Action;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.SimpleBehaviour;
 import jade.lang.acl.ACLMessage;
@@ -42,14 +46,27 @@ public class WaitForProposalRequest extends SimpleBehaviour {
                 nextTransition = ASKED_TO_PROPOSE_MOVE;
                 break;
             case ACLMessage.REQUEST:
-                logger.info("Asked to move!");
-                nextTransition = ASKED_TO_MOVE;
-                break;
-            case ACLMessage.INFORM:
-                logger.info("Other piece asked to move!");
-                nextTransition = OTHER_PIECE_ASKED_TO_MOVE;
+                if (asksMeToMove(message)) {
+                    logger.info("Asked me to move!");
+                    nextTransition = ASKED_TO_MOVE;
+                } else {
+                    logger.info("Asked other piece to move!");
+                }
+
+                turnContext.setCurrentMessage(message);
                 break;
         }
+    }
+
+    private boolean asksMeToMove(ACLMessage message) {
+        var asksMeToMove = false;
+        try {
+            var action = (Action) myAgent.getContentManager().extractContent(message);
+            asksMeToMove = action.getActor().equals(myAgent.getAID());
+        } catch (Codec.CodecException | OntologyException e) {
+            logger.warning("Failed to read message: " + e.getMessage());
+        }
+        return asksMeToMove;
     }
 
     @Override
