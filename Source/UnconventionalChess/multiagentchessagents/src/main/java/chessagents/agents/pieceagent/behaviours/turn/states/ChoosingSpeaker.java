1b470fd58b2e51584268e0439826c95c129c4443
Jordan Mackie <jmackie97@hotmail.com>
2019-11-02 21:36:26 +0000

    added TODOs to choosing speaker

--- Source/UnconventionalChess/multiagentchessagents/src/main/java/chessagents/agents/pieceagent/behaviours/turn/states/ChoosingSpeaker.java
+++ Source/UnconventionalChess/multiagentchessagents/src/main/java/chessagents/agents/pieceagent/behaviours/turn/states/ChoosingSpeaker.java
@@ -1,13 +1,10 @@
 package chessagents.agents.pieceagent.behaviours.turn.states;
 
-import chessagents.agents.ChessAgent;
 import chessagents.agents.pieceagent.PieceContext;
 import chessagents.agents.pieceagent.behaviours.turn.TurnContext;
 import chessagents.agents.pieceagent.behaviours.turn.fsm.PieceStateBehaviour;
 import chessagents.agents.pieceagent.pieces.PieceAgent;
 import chessagents.ontology.schemas.actions.BecomeSpeaker;
-import chessagents.ontology.schemas.concepts.Piece;
-import chessagents.ontology.schemas.predicates.IsSpeaker;
 import jade.content.lang.Codec;
 import jade.content.onto.OntologyException;
 import jade.content.onto.basic.Action;
@@ -16,10 +13,9 @@ import jade.core.behaviours.Behaviour;
 import jade.lang.acl.ACLMessage;
 import jade.util.Logger;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class ChoosingSpeaker extends Behaviour implements PieceStateBehaviour {
 
@@ -38,8 +34,7 @@ public class ChoosingSpeaker extends Behaviour implements PieceStateBehaviour {
     public void action() {
         if (receivedRequestFromEveryone()) {
             var speaker = chooseSpeaker();
-            informEveryoneOfSpeaker(speaker);
-            turnContext.setCurrentSpeaker(speaker);
+            sendResults(speaker);
         } else {
             var message = myAgent.receive();
 
@@ -56,48 +51,43 @@ public class ChoosingSpeaker extends Behaviour implements PieceStateBehaviour {
 
         try {
             var action = (Action) myAgent.getContentManager().extractContent(message);
-            var isSpeaker = (IsSpeaker) action.getAction();
 
-            // ClassCastException would have occurred before this point otherwise
-            isSpeakerProposal = true;
-        } catch (Codec.CodecException | ClassCastException | OntologyException e) {
+            isSpeakerProposal = action.getAction() instanceof BecomeSpeaker;
+
+            // Set content to object to avoid having to reextract later on
+            message.setContentObject(action);
+        } catch (Codec.CodecException | ClassCastException | OntologyException | IOException e) {
             logger.warning("Failed to extract content from message: " + e.getMessage());
         }
 
         return isSpeakerProposal;
     }
 
-    private void informEveryoneOfSpeaker(AID speaker) {
-        var rejections = new HashSet<ACLMessage>();
-        ACLMessage accept = null;
-
-        var
-
-        for (ACLMessage proposal : speakerProposals) {
-            if (proposal.getSender().equals(speaker)) {
-
-            } else {
-                var rejection = proposal.createReply();
-                proposal.setPerformative(ACLMessage.REJECT_PROPOSAL);
-            }
-        }
+    private void sendResults(AID speaker) {
+        // TODO work out how to allow agents to propose other pieces as speaker
+//        var rejections = speakerProposals.stream().filter(proposal -> proposal.getContentObject().)
     }
 
     private AID chooseSpeaker() {
+        // TODO
+        return null;
     }
 
     @Override
     public boolean done() {
+        // TODO
         return false;
     }
 
     @Override
     public int getNextTransition() {
+        // TODO
         return 0;
     }
 
     @Override
     public void reset() {
+        // TODO
         super.reset();
     }
 
