ae995b1d14427d8d206c498fe1a0731029ce2ee1
Jordan Mackie <jmackie97@hotmail.com>
2019-11-02 19:21:02 +0000

    add decision making to decide if requesting proposals

--- Source/UnconventionalChess/multiagentchessagents/src/main/java/chessagents/agents/pieceagent/behaviours/turn/states/DecideIfRequestingProposals.java
+++ Source/UnconventionalChess/multiagentchessagents/src/main/java/chessagents/agents/pieceagent/behaviours/turn/states/DecideIfRequestingProposals.java
@@ -2,16 +2,22 @@ package chessagents.agents.pieceagent.behaviours.turn.states;
 
 import chessagents.agents.pieceagent.PieceContext;
 import chessagents.agents.pieceagent.behaviours.turn.TurnContext;
+import chessagents.agents.pieceagent.behaviours.turn.fsm.PieceTransition;
 import chessagents.agents.pieceagent.pieces.PieceAgent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.SimpleBehaviour;
 
+import java.util.Random;
+
 import static chessagents.agents.pieceagent.behaviours.turn.fsm.PieceTransition.NOT_REQUESTING_PROPOSALS;
+import static chessagents.agents.pieceagent.behaviours.turn.fsm.PieceTransition.REQUESTING_PROPOSALS;
 
 public class DecideIfRequestingProposals extends SimpleBehaviour {
 
+    private final Random random = new Random();
     private final PieceContext pieceContext;
     private final TurnContext turnContext;
+    private PieceTransition pieceTransition = null;
 
     public DecideIfRequestingProposals(PieceAgent pieceAgent, PieceContext pieceContext, TurnContext turnContext) {
         super(pieceAgent);
@@ -21,7 +27,16 @@ public class DecideIfRequestingProposals extends SimpleBehaviour {
 
     @Override
     public void action() {
-        // TODO actual logic for deciding if requesting proposals or telling piece to move
+        if (turnContext.getDebateCycles() < pieceContext.getMaxDebateCycle() && requestingProposals()) {
+            pieceTransition = REQUESTING_PROPOSALS;
+        } else {
+            pieceTransition = NOT_REQUESTING_PROPOSALS;
+        }
+    }
+
+    private boolean requestingProposals() {
+        // TODO change from random chance to personality affected decision
+        return random.nextBoolean();
     }
 
     @Override
@@ -31,11 +46,12 @@ public class DecideIfRequestingProposals extends SimpleBehaviour {
 
     @Override
     public int onEnd() {
-        return NOT_REQUESTING_PROPOSALS.ordinal();
+        return (pieceTransition != null ? pieceTransition : NOT_REQUESTING_PROPOSALS).ordinal();
     }
 
     @Override
     public void reset() {
+        pieceTransition = null;
         super.reset();
     }
 }
