b0508f588d83651359c8ea7d72c715e22639c58c
Jordan Mackie <jmackie97@hotmail.com>
2020-01-05 16:53:36 +0000

    restore streams in personality

--- Source/UnconventionalChess/multiagentchessagents/src/main/java/chessagents/agents/pieceagent/personality/Personality.java
+++ Source/UnconventionalChess/multiagentchessagents/src/main/java/chessagents/agents/pieceagent/personality/Personality.java
@@ -38,26 +38,17 @@ public class Personality {
      * @return the set of actions considered with this personalities response to them
      */
     public Set<MoveResponse> getResponseToMoves(ChessPiece chessPiece, Set<PieceMove> actions, GameState gameState) {
-        Set<MoveResponse> set = new HashSet<>();
-        for (PieceMove action : actions) {
-            Set<MoveResponse> moveResponses = getMoveResponses(chessPiece, gameState, action);
-            for (MoveResponse moveRespons : moveResponses) {
-                set.add(moveRespons);
-            }
-        }
-        return set;
+        return actions.stream()
+                .map(action -> getMoveResponses(chessPiece, gameState, action))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
     }
 
     private Set<MoveResponse> getMoveResponses(ChessPiece chessPiece, GameState gameState, PieceMove action) {
-        Set<MoveResponse> set = new HashSet<>();
-        for (Trait trait : traits) {
-            Set<Value> values = trait.getAppealingValues();
-            for (Value value : values) {
-                MoveResponse moveResponse = value.getMoveResponse(chessPiece, gameState, action);
-                set.add(moveResponse);
-            }
-        }
-        return set;
+        return traits.stream()
+                .map(Trait::getAppealingValues)
+                .flatMap(values -> values.stream().map(value -> value.getMoveResponse(chessPiece, gameState, action)))
+                .collect(Collectors.toSet());
     }
 
     public Collection<Trait> getTraits() {
